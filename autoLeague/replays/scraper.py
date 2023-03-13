8c63a65c0595d96ee95c915c4db40b3fd66bd5eb
sykim1106 <53938323+hanueluni1106@users.noreply.github.com>
2023-03-13 16:35:01 +0900

    Add files via upload

--- autoLeague/replays/scraper.py
+++ autoLeague/replays/scraper.py
@@ -35,7 +35,7 @@ class ReplayScraper(object):
         self.replay_speed = replay_speed
         self.region = region
 
-    def run_client(self, replay_path):
+    def run_client(self, replay_path, start, end, speed, paused):
         args = [
             str(os.path.join(self.game_dir, "League of Legends.exe")),
             replay_path,
@@ -50,6 +50,57 @@ class ReplayScraper(object):
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             cwd=self.game_dir)
+        
+        '''
+        post_running : 리플레이 시작시간, 배속, 일시중지 여부 post 요청 진행 여부, 클라이언트가 리플레이를 돌리기 전까지 계속 요청.
+        리플레이 돌리고 post 성공하면 False 로 변환.
+        '''
+        post_running = True 
+
+        while post_running:
+            try:
+                time.sleep(1)
+                req = requests.post(
+                        'https://127.0.0.1:2999/replay/playback',
+                        headers={
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+                        data = json.dumps({
+                            "paused": paused,
+                            "seeking" : False,   
+                            "time": start - 5,
+                            "speed": speed
+                        }),
+                        verify=False
+                    )
+                #req = requests.get('https://127.0.0.1:2999/replay/playback' , verify = False)
+                if req.status_code == 200:
+                    print("상태코드 : ",req.status_code , "응답 : ",req)
+                    post_running = False
+            except:
+                time.sleep(1)
+                pass
+
+        '''
+        replay_running : 리플레이가 실행중인가에 대한 불린 ; 클라이언트가 end 시각까지만 실행하도록
+        '''     
+        replay_running = True    
+        while replay_running:
+            try:
+                timestamp = requests.get('https://127.0.0.1:2999/replay/playback', verify=False).json()['time']    
+
+                if  end <= timestamp <= end + 2:
+                    print(f"리플레이 정상 정지, 요청 종료 시각 : {end}s , 실제 종료 시각{timestamp}s")
+                    replay_running  = False
+
+            except:
+                time.sleep(1)
+                pass
+
+        #클라이언트 종료           
+        os.system("taskkill /f /im \"League of Legends.exe\"")       
+
 
     def run_scraper(self, output_path, end_time):
         time.sleep()
